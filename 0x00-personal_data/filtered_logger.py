1b1323de197dc75c76b567bcf760632a40fd111d
silgenius <olutademartin@gmail.com>
2024-11-11 13:21:04 +0000

    Implement solution to task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -14,6 +14,7 @@ Functions:
 
 import re
 from typing import List
+import logging
 
 
 def filter_datum(
@@ -25,3 +26,21 @@ def filter_datum(
     """a function called that returns the log message obfuscated"""
     pattern = r'(' + '|'.join(fields) + r')=(.*?)(' + separator + '|$)'
     return re.sub(pattern, r'\1=' + redaction + r'\3', message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ method to filter values in incoming log records"""
+        msg = super(RedactingFormatter, self).format(record) 
+        filter_msg = filter_datum(self.fields, self.REDACTION, msg, self.FORMAT)
+        return filter_msg
