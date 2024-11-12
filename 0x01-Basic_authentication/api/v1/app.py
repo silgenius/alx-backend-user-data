9fd6e708663394e0b389973ed6b9d342a465bd93
silgenius <olutademartin@gmail.com>
2024-11-12 13:39:28 +0000

    Implement solution to tasks

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,6 +13,12 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+auth = None
+auth = os.getenv('AUTH_TYPE')
+if auth:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -31,6 +37,26 @@ def unauthorized(error) -> str:
 def forbidden(error) -> str:
     """ Forbidden error handler"""
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    if not auth:
+        return
+
+    avail_path = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/'
+            ]
+    if not auth.require_auth(request.path, avail_path):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -16,13 +16,25 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
          returns False - path and excluded_paths
+         - assumes paths in excluded_paths contains '/' athe the end
         """
-        return False
+        if path and path[-1] != '/':
+            path += '/'
+
+        if excluded_paths and path in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
         returns None
         """
+        if request:
+            auth = request.headers.get('Authorization')
+            if auth:
+                return auth
+
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
