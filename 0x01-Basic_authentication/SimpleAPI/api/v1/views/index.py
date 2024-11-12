10dbdb90e0747fda33361e9ad6a504df508b2323
silgenius <olutademartin@gmail.com>
2024-11-12 10:22:31 +0000

    Implement solution to task 1

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def status_unautorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+       - unautorized status
+    """
+    abort(401)
