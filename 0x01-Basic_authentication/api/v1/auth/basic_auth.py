a6f60238b68c441967851b014b01822d83cdc21c
silgenius <olutademartin@gmail.com>
2024-11-13 01:43:33 +0000

    Implement soluton to task 8 - 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,10 @@
 """
 
 from .auth import Auth
+import base64
+from models.user import User
+from typing import TypeVar
+import hashlib
 
 
 class BasicAuth(Auth):
@@ -27,4 +31,57 @@ class BasicAuth(Auth):
                 else:
                     if auth_value[0] == 'Basic':
                         return auth_value[1]
+        return None
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+            ) -> str:
+        """returns the decoded value of a Base64 string"""
+        if base64_authorization_header:
+            if isinstance(base64_authorization_header, str):
+                try:
+                    b64_value = base64.b64decode(base64_authorization_header)
+                    # Decode from byte to str
+                    b64_value = b64_value.decode('utf-8')
+                except Exception:
+                    pass
+                else:
+                    return b64_value
+        return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str
+            ) -> (str, str):
+        """
+        returns the user email and password from the Base64 decoded value.
+        """
+        if decoded_base64_authorization_header:
+            if isinstance(decoded_base64_authorization_header, str):
+                try:
+                    name, pwd = decoded_base64_authorization_header.split(':')
+                except Exception:
+                    pass
+                else:
+                    return (name, pwd)
+        return (None, None)
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str
+            ) -> TypeVar('User'):
+        if user_email and user_pwd:
+            if isinstance(user_email, str) and \
+                    isinstance(user_pwd, str):
+                users = User.search()
+                if users and len(users) > 0:
+                    for user in users:
+                        user_pwd = hashlib.sha256(
+                                user_pwd.encode()
+                                ).hexdigest().lower()
+                        if user.email == user_email \
+                                and user._password == user_pwd:
+                            return user
         return None
