283ce700116ba8ece2d5acce7a78c52b36acdda3
silgenius <olutademartin@gmail.com>
2024-11-13 02:24:07 +0000

    Fix code error

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -100,5 +100,8 @@ class BasicAuth(Auth):
         auth_data = self.extract_base64_authorization_header(auth_header)
         auth_data = self.decode_base64_authorization_header(auth_data)
         user_credentials = self.extract_user_credentials(auth_data)
-        user_object = self.user_object_from_credentials(*user_credentials)
+        user_object = self.user_object_from_credentials(
+                user_credentials[0],
+                user_credentials[1]
+                )
         return user_object
