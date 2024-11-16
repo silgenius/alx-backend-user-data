fbeb97e6afad0dc254379e34bd5c610a13afd384
silgenius <olutademartin@gmail.com>
2024-11-16 05:03:53 +0000

    Refactor: Fix error in task

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+""" UserSession module
+"""
+
+import hashlib
+from models.base import Base
+
+
+class UserSession(Base):
+    """
+    UserSesson
+    """
+    def __init__(self, *args: list, **kwargs: dict):
+        """
+        Initialize user session
+        """
+        if args:
+            self.user_id = args[0]
+            self.session_id = args[1]
+
+        if kwargs and kwargs is not None:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
