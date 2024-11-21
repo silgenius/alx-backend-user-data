6a53373afc023fe71b5decea44d7a533d34df4b1
silgenius <olutademartin@gmail.com>
2024-11-21 15:42:09 +0000

    Properly annotate file

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,7 +8,7 @@ password and returns a salted, hashed password, which is a byte string.
 import bcrypt
 
 
-def hash_password(password):
+def hash_password(password: str) -> bytes:
     """
     Uses the package to perform the hashing
     """
