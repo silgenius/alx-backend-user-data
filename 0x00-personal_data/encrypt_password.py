80a8f55919b0ec6db8c038632bacf3d863ff98ee
silgenius <olutademartin@gmail.com>
2024-11-21 15:47:30 +0000

    Implement solution to task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -14,3 +14,12 @@ def hash_password(password: str) -> bytes:
     """
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Use bcrypt to validate that the provided
+    password matches the hashed password
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+
